fb31ddfa58a443bdab72daa7159636be48cea64e
Sergi0let <s.vashkevych@gmail.com>
2022-07-12 10:28:34 +0300

    init

---
+++ js/arrays/task4/index.js
@@ -0,0 +1,20 @@
+// Hlib
+// const withdraw = (clients, balances, client, amount) => {
+// remote logic from if
+// get rid of dubplicated code
+// don't use find
+
+// good 4
+//   balances[clients.indexOf(client)];
+//   if (balances.find((item, index) => clients.indexOf(client) === index) < amount) {
+//     return -1;
+//   }
+//   return balances.find((item, index) => clients.indexOf(client) === index) - amount;
+// };
+
+const withdraw = (clients, balances, client, amount) => {
+  const currentClient = clients.indexOf(client);
+  return balances[clients.indexOf(client)] >= amount
+    ? (balances[clients.indexOf(client)] -= amount)
+    : -1;
+};
