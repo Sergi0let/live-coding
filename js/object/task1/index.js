8ff6de8f88fa247311b9eec4d97c37ca7f8e5340
Sergi0let <s.vashkevych@gmail.com>
2022-07-17 15:06:15 +0300

    init

--- js/object/task1/index.js
+++ js/object/task1/index.js
@@ -1,60 +1,82 @@
-// input obj string value
-// output obj
+// // input obj string value
+// // output obj
 
-/* eslint-disable prefer-object-spread */
-/* eslint-disable no-param-reassign */
-function addPropertyV1(obj, key, value) {
-  obj[key] = value;
-  return obj;
-}
-// examples
-const transaction = {
-  value: 170,
-};
-// console.log(addPropertyV1(transaction, 'currency', 'USD')); // ==> { value: 170, currency: 'USD' }
+// /* eslint-disable prefer-object-spread */
+// /* eslint-disable no-param-reassign */
+// function addPropertyV1(obj, key, value) {
+//   obj[key] = value;
+//   return obj;
+// }
+// // examples
+// const transaction = {
+//   value: 170,
+// };
+// // console.log(addPropertyV1(transaction, 'currency', 'USD')); // ==> { value: 170, currency: 'USD' }
 
-// #2
-function addPropertyV2(obj, key, value) {
-  // input : obj;
-  // input: old obj with new proparty
-  return Object.assign(obj, { [key]: value });
-}
-// examples
-const transaction2 = {
-  value: 170,
-};
-// console.log(addPropertyV2(transaction2, 'currency', 'USD')); // ==> { value: 170, currency: 'USD' }
-// console.log(transaction2);
-// console.log(addPropertyV2(transaction2, 'name', 'John')); // ==> {value: 170, currency: 'USD', name: 'John'}
-// console.log(addPropertyV2(transaction2, 'city', 'New York')); // ==> {value: 170, currency: 'USD', name: 'John', city: 'New York'}
+// // #2
+// function addPropertyV2(obj, key, value) {
+//   // input : obj;
+//   // input: old obj with new proparty
+//   return Object.assign(obj, { [key]: value });
+// }
+// // examples
+// const transaction2 = {
+//   value: 170,
+// };
+// // console.log(addPropertyV2(transaction2, 'currency', 'USD')); // ==> { value: 170, currency: 'USD' }
+// // console.log(transaction2);
+// // console.log(addPropertyV2(transaction2, 'name', 'John')); // ==> {value: 170, currency: 'USD', name: 'John'}
+// // console.log(addPropertyV2(transaction2, 'city', 'New York')); // ==> {value: 170, currency: 'USD', name: 'John', city: 'New York'}
 
-// #3
-function addPropertyV3(obj, key, value) {
-  // input :  {}, obj,  obj
-  // input: new obj
-  return Object.assign({}, obj, { [key]: value });
-}
-// examples
+// // #3
+// function addPropertyV3(obj, key, value) {
+//   // input :  {}, obj,  obj
+//   // input: new obj
+//   return Object.assign({}, obj, { [key]: value });
+// }
+// // examples
+// // const transaction3 = {
+// //   value: 170,
+// // };
+// // const res1 = addPropertyV3(transaction3, 'currency', 'USD');
+// // const res2 = addPropertyV3(res1, 'amount', '300');
+// // console.log(res1);
+// // console.log('result 1c', res2);
+// // console.log('result 2 ', res2);
+// // console.log('obj after', transaction3);
+// // #4
+// function addPropertyV4(obj, key, value) {
+//   return { ...obj, [key]: value };
+// }
+// // examples
 // const transaction3 = {
 //   value: 170,
 // };
-// const res1 = addPropertyV3(transaction3, 'currency', 'USD');
-// const res2 = addPropertyV3(res1, 'amount', '300');
+// const res1 = addPropertyV4(transaction3, 'currency', 'USD');
+// const res2 = addPropertyV4(res1, 'amount', '300');
 // console.log(res1);
-// console.log('result 1c', res2);
-// console.log('result 2 ', res2);
+// console.log('result 1 -', res2);
+// console.log('result 2 -', res2);
 // console.log('obj after', transaction3);
-// #4
-function addPropertyV4(obj, key, value) {
-  return { ...obj, [key]: value };
-}
-// examples
-const transaction3 = {
-  value: 170,
+const getRandomNumbers = (length, from, to) => {
+  if (from > to) {
+    return null;
+  }
+  const start = Math.ceil(from);
+  const finish = Math.ceil(to);
+  return finish === start
+    ? null
+    : new Array(length).fill().map(el => Math.floor(Math.random() * (finish - start)) + start);
 };
-const res1 = addPropertyV4(transaction3, 'currency', 'USD');
-const res2 = addPropertyV4(res1, 'amount', '300');
-console.log(res1);
-console.log('result 1 -', res2);
-console.log('result 2 -', res2);
-console.log('obj after', transaction3);
+
+// examples
+console.log(getRandomNumbers(5, 1.4, 3.22)); // ==> [2, 2, 2, 3, 2]
+getRandomNumbers(5, 1.4, 3.22); // ==> [3, 2, 2, 2, 2]
+getRandomNumbers(5, 1.4, 3.22); // ==> [3, 3, 2, 3, 2]
+getRandomNumbers(3, 2.4, 4.5); // ==> [4, 3, 4]
+getRandomNumbers(3, 1.9, 2.5); // ==> [2, 2, 2]
+
+console.log(getRandomNumbers(7, 1.4, 1.9)); // ==> null
+console.log(getRandomNumbers(7, 2.11, 2.9)); // ==> null
+console.log(getRandomNumbers(1, 2.5, 0.9)); // ==> null
+console.log(getRandomNumbers(1, 2.5, 0.9)); // ==> null
---
+++ js/object/task2/index.js
@@ -0,0 +1,133 @@
+// in: array, array
+// out: obj
+// algo
+// 1. create new obj
+// 2. iterate keylist
+// 3. put key/value to the obj
+/*
+function buildObject(keysList, valuesList) {
+  const obj = {};
+
+  for (let index = 0; index < keysList.length; index += 1) {
+    obj[keysList[index]] = valuesList[index];
+  }
+  return obj;
+}
+*/
+/*
+function buildObject(keysList, valuesList) {
+  return keysList.reduce((obj, key, index) => {
+    obj[key] = valuesList[index];
+    return obj;
+  }, {});
+}
+*/
+
+// function buildObject(keysList, valuesList) {
+//   return keysList.reduce((obj, key, index) => ({ ...obj, [key]: valuesList[index] }), {});
+// }
+
+// examples
+/*
+const keys = ['name', 'address', 'age', 'name'];
+const values = ['Bob', 'Ukraine', 34, 'James'];
+const result = buildObject(keys, values); // ==> { name: 'Bob', address: 'Ukraine', age: 34 }
+console.log(result);
+
+const keys2 = ['name', 'obj', null, 98];
+const values2 = ['Bob', { price: 100, status: true }, 34];
+const result2 = buildObject(keys2, values2); // ==> {0: 'Ukraine', 98: undefined, name: 'Bob', null: 34}
+console.log(result2);
+
+const keys3 = ['hobbies', 'work'];
+const values3 = ['Painting', true, 34, 'Ukraine'];
+const result3 = buildObject(keys3, values3); // ==> {hobbies: 'Painting', work: true}
+console.log(result3);
+
+const keys4 = ['name', 'address', 'age'];
+const values4 = ['Bob'];
+const result4 = buildObject(keys4, values4); // ==> {name: 'Bob', address: undefined, age: undefined}
+console.log(result4);
+*/
+
+// ------------------------------------------
+// in obj, obj
+// out: Boolean
+
+// logo:
+// 1. get keys1 , keys2
+// 2. if Length 1 ! == length2 ==> false
+// 3. iterate keys1
+// 4. compare key1/value1 with key2/value2
+// 5.if (step 4=== false) ==> false
+
+/*
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
+}
+*/
+
+/*
+function compareObjects(obj1, obj2) {
+  const objKeys = obj => Object.keys(obj).join('');
+  const objValues = obj => Object.values(obj).join();
+
+  return objKeys(obj1) === objKeys(obj2) && objValues(obj1) === objValues(obj2);
+}
+*/
+/*
+function compareObjects(obj1, obj2) {
+  const createObjString = obj => Object.entries(obj).flat().join('');
+  return createObjString(obj1) === createObjString(obj2);
+}
+*/
+
+const objKeys = obj => Object.keys(obj);
+const entriesObj = obj => Object.entries(obj).flat();
+
+function compareObjects(obj1, obj2) {
+  if (objKeys(obj1).length !== objKeys(obj2).length) {
+    return false;
+  }
+
+  return entriesObj(obj1).every(elem => entriesObj(obj2).includes(elem));
+}
+
+// // examples
+const obj1 = {
+  name: 'Tom',
+  age: 33,
+  email: null,
+};
+
+const obj2 = {
+  age: 33,
+  email: null,
+  name: 'Tom',
+};
+
+const obj3 = {
+  name: 'Bob',
+  age: 17,
+};
+
+const obj4 = {
+  name: 'Bob',
+  age: 17,
+};
+const obj5 = {
+  name: 'Bob',
+  age: 17,
+};
+
+const obj6 = {
+  addres: 'Lviv',
+  name: 'Bob',
+  age: 17,
+};
+
+console.log('res 1 ', compareObjects(obj1, obj2)); // ==> true
+console.log('res 2 ', compareObjects(obj2, obj3)); // ==> false
+console.log('res 3 ', compareObjects(obj1, obj4)); // ==> false
+console.log('res 4 ', compareObjects(obj3, obj4)); // ==> true
+console.log('res 5 ', compareObjects(obj5, obj6)); // ==> false
